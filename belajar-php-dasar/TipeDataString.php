889ec6a4b255708769b275620eb1b12753d1698f
adenafil <nafilie9@gmail.com>
2024-01-27 14:23:46 +0700

    8. Tipe Data String

---
+++ belajar-php-dasar/TipeDataString.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'Nama : ';
+echo 'Ade Nafil Firmansah';
+echo "\n";
+
+echo "Nama : ";
+echo "Ade\t Nafil\t Firmansah\n";
+echo "\"real\"";
+
+echo <<<ADE
+    Ini adalah contoh string sangat
+    panjang, dan juga gak perlu ngetik ENTER secara
+    manual, "bisa quote" juga
+    
+ADE;
+
+echo <<<"ade"
+    Ini adalah contoh string yang sangat
+    panjang, dan juga gak perlu ngetik ENTER secara
+    manual, "bisa quote" juga
+ade;
