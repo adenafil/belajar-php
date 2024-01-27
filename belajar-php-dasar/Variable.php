c02d8abd890b096e43309b0d61aec5edf8cb733a
adenafil <nafilie9@gmail.com>
2024-01-27 14:40:42 +0700

    9. Variable

---
+++ belajar-php-dasar/Variable.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama = "Ade";
+$age = 30;
+
+echo "Nama : ";
+echo $nama;
+echo "\n";
+echo "Age : ";
+echo $age;
+echo "\n";
+
+# Variable variables
+
+$name = "ade";
+$$name = "ganteng";
+echo "\$name = ";
+echo $name;
+echo "\n";
+echo "\$ade = ";
+echo $ade;
+echo "\n";
