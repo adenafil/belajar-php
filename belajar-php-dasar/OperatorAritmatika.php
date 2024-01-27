f904050d2d45a82b2b7ff477bb8594aece01553e
adenafil <nafilie9@gmail.com>
2024-01-27 16:29:59 +0700

    13. Operator Aritmatika

---
+++ belajar-php-dasar/OperatorAritmatika.php
@@ -0,0 +1,36 @@
+<?php
+
+# Penjumlahan
+$result = 10 + 10;
+var_dump($result);
+
+# Persisaan hasil bagi
+$result = 100 % 3;
+var_dump($result);
+
+# Konversi menjadi minus
+$minusResult = -$result;
+var_dump($minusResult);
+
+#result positif
+$positifResult = +$result;
+var_dump($positifResult);
+
+$a = 10;
+$b = 3;
+
+# pengurangan
+$pengurangan = $a - $b;
+var_dump($pengurangan);
+
+# pembagian
+$pembagian = $a / $b;
+var_dump($pembagian);
+
+# perkalian
+$perkalian = $a * $b;
+var_dump($perkalian);
+
+# perpangkatan
+$perpangkatan = $a ** $b;
+var_dump($perpangkatan);
