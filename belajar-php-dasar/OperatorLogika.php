8c9476875c5078ed663abbe4252f7b9e2967cc43
adenafil <nafilie9@gmail.com>
2024-01-28 16:06:05 +0700

    16. Operator Logika

---
+++ belajar-php-dasar/OperatorLogika.php
@@ -0,0 +1,8 @@
+<?php
+
+var_dump(true && true); # true
+var_dump(true && false); # false
+var_dump(true || false); # true
+var_dump(true || true); # true
+var_dump(true XOR true); # false
+var_dump(!false); #TRUE
