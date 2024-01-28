e5159851b14e515cc8c982536335bbd1cecd78ad
adenafil <nafilie9@gmail.com>
2024-01-28 15:52:31 +0700

    15. Operator Perbandingan

---
+++ belajar-php-dasar/OperatorPerbandingan.php
@@ -0,0 +1,12 @@
+<?php
+
+#Sama dengan
+var_dump(10 == "10"); # true
+var_dump(10 === "10"); # false
+var_dump(10 != "10"); #false
+var_dump(10 <> "10"); # false
+var_dump(10 !== "10"); # true
+var_dump(10 < "10"); # false
+var_dump(10 <= "10"); # true
+var_dump(10 > "10"); # false
+var_dump(10 >= "10"); # true
