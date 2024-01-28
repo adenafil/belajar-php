463992cc0a5059135975ecbdda450eb45dfe10b7
adenafil <nafilie9@gmail.com>
2024-01-28 16:19:48 +0700

    17. IncrementDanDecrement

---
+++ belajar-php-dasar/IncrementDanDecrement.php
@@ -0,0 +1,24 @@
+<?php
+
+# preincrement
+
+$a = 10;
+$b = ++$a;
+
+var_dump($a);
+var_dump($b);
+
+# postincrement
+
+$a = 10;
+$b = $a++;
+
+var_dump($a);
+var_dump($b);
+
+# tes lagi
+
+$ade = 100;
+
+var_dump($ade++);
+var_dump(++$ade);
