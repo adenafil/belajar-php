932dd8bdc2e6b6b19f80963327dad0f9444a25e4
adenafil <nafilie9@gmail.com>
2024-01-29 12:16:38 +0700

    31. Function

---
+++ belajar-php-dasar/Function.php
@@ -0,0 +1,22 @@
+<?php
+
+function sayHello()
+{
+    echo "Hello Function\n";
+}
+
+sayHello();
+sayHello();
+
+$buat = true;
+
+if ($buat) {
+    $s = "hayyuk";
+    function hi()
+    {
+        echo "Hi\n";
+    }
+}
+
+hi();
+echo $s;
