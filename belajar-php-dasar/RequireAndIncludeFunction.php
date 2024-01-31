914e1a509d9bc87124a48bdc21519350acaf23f6
adenafil <nafilie9@gmail.com>
2024-01-31 14:38:00 +0700

    43. Require And Include

---
+++ belajar-php-dasar/RequireAndIncludeFunction.php
@@ -0,0 +1,13 @@
+<?php
+
+// include ".\Lib\MyFunction.php";
+// require ".\Lib\MyFunction.php";
+include_once ".\Lib\MyFunction.php";
+include_once ".\Lib\MyFunction.php";
+
+
+
+echo sayHello("Ade", "Nafil");
+echo sayHello("Ade", "Nafil");
+
+echo "is this code be run ?";
