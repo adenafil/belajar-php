56fc7a2ae573043ae9075f87ea78bff5f8d3e340
adenafil <nafilie9@gmail.com>
2024-01-31 17:07:31 +0700

    52. Menentukan View

--- studi-kasus-php-dasar-aplikasi-todolist/App.php
+++ studi-kasus-php-dasar-aplikasi-todolist/App.php
@@ -4,5 +4,8 @@ require_once "Model/TodoList.php";
 require_once "BusinessLogic/AddTodoList.php";
 require_once "BusinessLogic/ShowTodoList.php";
 require_once "BusinessLogic/RemoveTodoList.php";
+require_once "view/ViewAddTodoList.php";
+require_once "view/ViewRemoveTodoList.php";
+require_once "view/ViewShowTodoList.php";
 
 echo "Aplikasi TodoList\n";
