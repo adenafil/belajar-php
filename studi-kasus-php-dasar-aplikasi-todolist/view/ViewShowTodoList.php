82523e007880b60fa633ff695302eced9d6cf62c
adenafil <nafilie9@gmail.com>
2024-02-01 05:40:00 +0700

    60. View Menampilkan TodoList

--- studi-kasus-php-dasar-aplikasi-todolist/view/ViewShowTodoList.php
+++ studi-kasus-php-dasar-aplikasi-todolist/view/ViewShowTodoList.php
@@ -1,6 +1,42 @@
 <?php
 
+require_once "./BusinessLogic/ShowTodoList.php";
+require_once "./BusinessLogic/AddTodoList.php";
+require_once "./BusinessLogic/RemoveTodoList.php";
+require_once "./Helper/Input.php";
+
 function viewShowTodoList()
 {
+    while (true)
+    {
+        echo "TodoList";
+
+        showTodoList();
     
+        echo "MENU\n";
+        echo "1. Tamabah Todo\n";
+        echo "2. Hapus Todo\n";
+        echo "3. Keluar\n";
+    
+        $pilihan = input("Pilih");
+    
+        if ($pilihan == "1")
+        {
+            viewAddTodoList();
+        }
+        else if ($pilihan == "2")
+        {
+            viewRemoveTodoList();
+        }
+        else if ($pilihan == "x")
+        {
+            break;
+        }
+        else
+        {
+            echo "Pilihan tidak dimengerti\n";
+        }
+    }
+
+    echo "Sampai Jumpa Lagi\n";
 }
