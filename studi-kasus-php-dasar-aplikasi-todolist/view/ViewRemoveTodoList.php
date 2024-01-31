f4c027b3a22e0f560a557688e48d66244c0fe0f9
adenafil <nafilie9@gmail.com>
2024-02-01 06:08:40 +0700

    64. View Menghapus TodoList

--- studi-kasus-php-dasar-aplikasi-todolist/view/ViewRemoveTodoList.php
+++ studi-kasus-php-dasar-aplikasi-todolist/view/ViewRemoveTodoList.php
@@ -1,6 +1,31 @@
 <?php
 
+require_once "./Helper/Input.php";
+require_once "./BusinessLogic/RemoveTodoList.php";
+
 function viewRemoveTodoList()
 {
+    echo "MENGHAPUS TODO\n";
+
+    $pilihan = input("Nomor (x untuk batalkan)");
+
+    if ($pilihan == "x")
+    {
+        echo "Batal menghapus todo\n";
+    }
+    else
+    {
+        $success = removeTodoList($pilihan);
+
+        if ($success)
+        {
+            echo "Sukses menghapus todo nomor $pilihan\n";
+        }
+        else
+        {
+            echo "Gagal menghapus todo nomor $pilihan";
+        }
     
+    }
+
 }
