4870a1a624fe016e1838b92acb481bf20203cef3
adenafil <nafilie9@gmail.com>
2024-02-01 06:34:39 +0700

    66. Test Seluruh Aplikasi

--- studi-kasus-php-dasar-aplikasi-todolist/App.php
+++ studi-kasus-php-dasar-aplikasi-todolist/App.php
@@ -7,5 +7,9 @@ require_once "BusinessLogic/RemoveTodoList.php";
 require_once "view/ViewAddTodoList.php";
 require_once "view/ViewRemoveTodoList.php";
 require_once "view/ViewShowTodoList.php";
+require_once "Helper/Input.php";
 
 echo "Aplikasi TodoList\n";
+echo __DIR__ . PHP_EOL;
+
+viewShowTodoList();
