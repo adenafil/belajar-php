bbf266f2f4cb46f1edee1098314b40dd76cf952a
adenafil <nafilie9@gmail.com>
2024-01-31 17:41:32 +0700

    56. Test Menambah TodoList

---
+++ studi-kasus-php-dasar-aplikasi-todolist/Test/TestAddTodoList.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "./Model/TodoList.php";
+require_once "./BusinessLogic/AddTodoList.php";
+
+addTodoList("ade");
+addTodoList("nafil");
+addTodoList("firmansah");
+
+var_dump($todoList);
+
