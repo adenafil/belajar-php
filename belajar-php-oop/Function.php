fd8c8cd77d4fee7c81e8c4b55be9d1947ca9e58e
adenafil <nafilie9@gmail.com>
2024-02-01 10:01:55 +0700

    73. Function.

---
+++ belajar-php-oop/Function.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "./data/Person.php";
+
+$person = new Person();
+$person->name = "ade";
+$person->address = "berau";
+
+$person->sayHello("napil");
--- belajar-php-oop/data/Person.php
+++ belajar-php-oop/data/Person.php
@@ -5,4 +5,9 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "indonesia";
+
+    function sayHello(string $name): void
+    {
+        echo "Hello $name\n";
+    }
 }
