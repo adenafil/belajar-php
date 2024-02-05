603bde4fe48ef2169f80a14e3bd54461a9c8d510
adenafil <nafilie9@gmail.com>
2024-02-05 16:00:46 +0700

    98.Final Function

--- belajar-php-oop/data/SocialMedia.php
+++ belajar-php-oop/data/SocialMedia.php
@@ -7,11 +7,17 @@ class SocialMedia
 
 final class Facebook extends SocialMedia
 {
-
+    final public function login(string $username, string $password): bool
+    {
+        return true;
+    }
 }
 
 // Error
 // class FakeFacebook extends Facebook 
 // {
-
+//     public function login(): void
+//     {
+//         return false;
+//     }
 // }
