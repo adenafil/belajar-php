bdc326faf12f134f92288c1d852d78cee1a7b8c2
adenafil <nafilie9@gmail.com>
2024-02-08 18:58:07 +0700

    146.View: Menampilkan Todolist

--- studi-kasus-php-oop-todolist/view/TodolistView.php
+++ studi-kasus-php-oop-todolist/view/TodolistView.php
@@ -2,11 +2,54 @@
 
 namespace View
 {
+    require_once __DIR__ . "/../Service/TodolistService.php";
+    require_once __DIR__ . "/../Helper/InputHelper.php";
+
+    use Helper\InputHelper;
+    use Service\TodolistService;
+    use Service\TodolistServiceImpl;
     class TodolistView
     {
+        private TodolistService $todolistService;
+
+        public function __construct(TodolistService $todolistService)
+        {
+            $this->todolistService = $todolistService;
+        }
+
+
         function showTodolist(): void
         {
+            while (true)
+            {
+                $this->todolistService->showTodolist();
 
+                echo "MENU\n";
+                echo "1. Tamabah Todo\n";
+                echo "2. Hapus Todo\n";
+                echo "x. Keluar\n";
+
+                $pilihan = InputHelper::input("Pilih");
+
+                if ($pilihan == "1")
+                {
+                    $this->addTodolist();
+                }
+                else if ($pilihan == "2")
+                {
+                    $this->removeTodolist();
+                }
+                else if ($pilihan == "x")
+                {
+                    break;
+                }
+                else
+                {
+                    echo "Pilihan tidak dimengerti\n";
+                }
+            }
+
+            echo "Sampai Jumpa Lagi\n";
         }
 
         function addTodolist(): void
