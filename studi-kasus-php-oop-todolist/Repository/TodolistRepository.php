0b69673eb92db2a94e83f1301338dfe6a69bd3b9
adenafil <nafilie9@gmail.com>
2024-02-08 16:32:59 +0700

    140. Repository dan Service: Menampilkan Todolist

--- studi-kasus-php-oop-todolist/Repository/TodolistRepository.php
+++ studi-kasus-php-oop-todolist/Repository/TodolistRepository.php
@@ -4,6 +4,7 @@ namespace Repository
 {
 
     use Entity\Todolist;
+    use Override;
 
     interface TodolistRepository
     {
@@ -12,5 +13,28 @@ namespace Repository
         function remove(int $number): bool;
 
         function findAll(): array;
+    }
+
+    class TodolistRepositoryImpl implements TodolistRepository
+    {
+        private array $todoList = array();
+        #[Override]
+        function save(Todolist $todolist): void
+        {
+            // TODO: Implement save() method.
+        }
+
+        #[Override]
+        function remove(int $number): bool
+        {
+            // TODO:  remove() method.
+            return false;
+        }
+
+        #[Override]
+        function findAll(): array
+        {
+            return $this->todoList;
+        }
     }
 }
--- studi-kasus-php-oop-todolist/Service/TodolistService.php
+++ studi-kasus-php-oop-todolist/Service/TodolistService.php
@@ -2,6 +2,9 @@
 
 namespace Service
 {
+    use Override;
+    use Repository\TodolistRepository;
+
     interface TodolistService
     {
         function showTodolist(): void;
@@ -9,5 +12,44 @@ namespace Service
         function addTodolist(string $todo): void;
 
         function removeTodolist(int $number);
+    }
+
+    class TodolistServiceImpl implements TodolistService
+    {
+        private TodolistRepository $todolistRepository;
+
+        /**
+         * @param TodolistRepository $todolistRepository
+         */
+        public function __construct(TodolistRepository $todolistRepository)
+        {
+            $this->todolistRepository = $todolistRepository;
+        }
+
+
+        #[Override]
+        function showTodolist(): void
+        {
+
+            echo  "TODOLIST\n";
+            $todolist = $this->todolistRepository->findAll();
+
+            foreach ($todolist as $number => $value)
+            {
+                echo "$number. $value\n";
+            }
+        }
+
+        #[Override]
+        function addTodolist(string $todo): void
+        {
+            // TODO: Implement addTodolist() method.
+        }
+
+        #[Override]
+        function removeTodolist(int $number)
+        {
+            // TODO: Implement removeTodolist() method.
+        }
     }
 }
