3623b077b3731936401a8cf2fb13e445d7cb0b3f
adenafil <nafilie9@gmail.com>
2024-02-08 19:08:48 +0700

    147. Test View Menampilkan Todolist

---
+++ studi-kasus-php-oop-todolist/Test/TodolistViewTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . "/../view/TodolistView.php";
+require_once __DIR__ . "/../Service/TodolistService.php";
+require_once __DIR__ . "/../Repository/TodolistRepository.php";
+require_once __DIR__ . "/../Entity/Todolist.php";
+
+use View\TodolistView;
+use Service\TodolistServiceImpl;
+use Repository\TodolistRepositoryImpl;
+use Entity\Todolist;
+function testViewShowTodolist(): void
+{
+    $todolistService = new TodolistServiceImpl(new TodolistRepositoryImpl());
+    $todolistService->addTodolist("Belajar PHP");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+
+    $todolistView = new TodolistView($todolistService);
+    $todolistView->showTodolist();
+
+}
+
+testViewShowTodolist();
