6eae0e1ba61624fc9264ed9f592ed86da680f0c8
adenafil <nafilie9@gmail.com>
2024-02-08 14:19:15 +0700

    127. Comma di Parameter List

---
+++ belajar-php-8/CommaParameterList.php
@@ -0,0 +1,40 @@
+<?php
+
+function sayHello(string $first, string $last)
+{
+
+}
+
+function sum(int ...$values)
+{
+
+}
+
+sayHello(
+    "Ade",
+    "Nafil",
+);
+
+sum(
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+);
+
+$array = [
+    "first" => "ade",
+    "middle" => "ade",
+    "last" => "ade",
+    "address" => "ade",
+    "country" => "ade",
+    "birthday" => "ade",
+];
