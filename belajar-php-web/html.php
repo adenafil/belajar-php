b6408f9921b1d5c95c389ac1e3b746e11c40ff1a
adenafil <nafilie9@gmail.com>
2024-02-10 21:54:22 +0700

    184. Integrasi dengan HTML

---
+++ belajar-php-web/html.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Hello PHP dan HTML";
+$body = "Hello PHP dan HTML";
+?>
+
+<html>
+<head>
+    <title><?=  $title; ?></title>
+</head>
+<body>
+<h1 style="color: brown"><?=$body;?></h1>
+</body>
+</html>
