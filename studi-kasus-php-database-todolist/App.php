800f79e88c1efde285b12a524b9d6876c4e3f04f
adenafil <nafilie9@gmail.com>
2024-02-10 18:47:36 +0700

    175. Test Seluruh Aplikasi

--- studi-kasus-php-database-todolist/App.php
+++ studi-kasus-php-database-todolist/App.php
@@ -5,14 +5,16 @@ require_once __DIR__ . "/Helper/InputHelper.php";
 require_once __DIR__ . "/Repository/TodolistRepository.php";
 require_once __DIR__ . "/Service/TodolistService.php";
 require_once __DIR__ . "/view/TodolistView.php";
+require_once __DIR__ . "/Config/Database.php";
 
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 use View\TodolistView;
+use Config\Database;
 
 echo "Aplikasi TodoList\n";
 
-$todolistRepository = new TodolistRepositoryImpl();
+$todolistRepository = new TodolistRepositoryImpl(Database::getConnection());
 $todolistService = new TodolistServiceImpl($todolistRepository);
 $todolistView = new TodolistView($todolistService);
 $todolistView->showTodolist();
