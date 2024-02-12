a4faf48359162ef161e743567cd9d9dd09a369b5
adenafil <nafilie9@gmail.com>
2024-02-12 16:34:35 +0700

    209. Upgrade versi Library

--- belajar-php-composer/HelloLibrary.php
+++ belajar-php-composer/HelloLibrary.php
@@ -5,4 +5,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 use ProgrammerZamanNow\Belajar\Customer;
 
 $customer = new Customer("ade");
-echo $customer->sayHello("firmansah");
+echo $customer->sayHello("firmansah") . PHP_EOL;
+echo $customer->sayHello() . PHP_EOL;
