a1de4dc43a4fd8f32f03da5ca8245cb7fc6848fd
adenafil <nafilie9@gmail.com>
2024-02-16 19:53:24 +0700

    219. Test Dependency

--- belajar-php-unit-test/tests/CounterTest.php
+++ belajar-php-unit-test/tests/CounterTest.php
@@ -3,6 +3,9 @@
 namespace ProgrammerZamanNow\Test;
 
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Attributes\Depends;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Constraint\Count;
 use PHPUnit\Framework\TestCase;
 
 class CounterTest extends TestCase
@@ -21,14 +24,32 @@ class CounterTest extends TestCase
 
     }
 
-    /**
-     * @test
-     */
+
+    #[Test]
     public function increment()
     {
         $counter = new Counter();
         $counter->increment();
         self::assertEquals(1, $counter->getCounter());
     }
+
+    public function testFirst(): Counter
+    {
+        $counter = new Counter();
+        $counter->increment();
+        $this->assertEquals(1, $counter->getCounter());
+        return $counter;
+    }
+
+    /**
+     * @depends testFirst
+     */
+
+    public function testSecond(Counter $counter): void
+    {
+        $counter->increment();
+        $this->assertEquals(2, $counter->getCounter());
+    }
+
 
 }
