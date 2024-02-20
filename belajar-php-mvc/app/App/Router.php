abd4e5785c290a00d86d970fc77f2cf91410f841
adenafil <nafilie9@gmail.com>
2024-02-20 19:28:04 +0700

    239. Controller

--- belajar-php-mvc/app/App/Router.php
+++ belajar-php-mvc/app/App/Router.php
@@ -4,5 +4,49 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\App;
 
 class Router
 {
+    private static array $routes = [];
 
+    public static function add(string  $method,
+                                string $path,
+                                string $controller,
+                                string $function
+                                ): void
+    {
+        self::$routes[] = [
+            'method' => $method,
+            'path' => $path,
+            'controller' => $controller,
+            'function' => $function
+        ];
+    }
+
+    public static function run(): void
+    {
+        $path = "/";
+        // Mengecek apakah di web nya ada path info
+        if (isset($_SERVER['PATH_INFO'])) $path = $_SERVER['PATH_INFO'];
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        // jika ada, cek dulu apakah di array routes memiliki path tersebut
+        foreach (self::$routes as $route)
+        {
+            // Jika ada yah masuk sini
+            if ($path == $route['path'] && $method == $route['method'])
+            {
+//                echo "CONTROLLER : " . $route['controller'] . ", FUNCTION : " . $route['function'];
+//                echo "</br>method : " . $route['method'] . ", path : " . $route['path'];
+
+                $controller = new $route['controller'];
+
+                $function = $route['function'];
+                $controller->$function();
+
+                return;
+            }
+        }
+
+        // Kalau gk ada masuk sini
+        http_response_code(404);
+        echo "CONTROLLER NOT FOUND";
+    }
 }
--- belajar-php-mvc/public/index.php
+++ belajar-php-mvc/public/index.php
@@ -3,9 +3,11 @@
 require_once  __DIR__ . '/../vendor/autoload.php';
 
 use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
 
-Router::add('GET', '/', 'HomeController', 'index');
-Router::add('GET', '/hello', 'HelloController', 'hello');
-Router::add('GET', '/world', 'WorldoController', 'world');
+Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/hello', HomeController::class, 'hello');
+Router::add('GET', '/world', HomeController::class, 'world');
+Router::add('GET', '/about', HomeController::class, 'about');
 
 Router::run();
