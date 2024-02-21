43dd0bc6ff85b25623e8e83d17c852dd32399299
adenafil <nafilie9@gmail.com>
2024-02-21 21:49:56 +0700

    255. View Template

--- php-login-management/app/Controller/HomeController.php
+++ php-login-management/app/Controller/HomeController.php
@@ -6,47 +6,10 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\App\View;
 
 class HomeController
 {
-    function index(): void
-    {
-        $model = [
-            "title" => "Belajar PHP MVC",
-            "content" => "Selamat Belajar PHP MVC dari PZN, masuk pak eko 😎"
-        ];
-
-
-        View::render('Home/index', $model);
-
-    }
-
-    function hello(): void
-    {
-        echo "HomeController.hello()";
-    }
-
-    function world(): void
-    {
-        echo "HomeController.world()";
-    }
-
-    function about(): void
-    {
-        echo "Author : Ade Nafil Firmansah";
-    }
-
-    function login(): void
-    {
-        $request = [
-            "username" => $_POST['username'],
-            "password" => $_POST['password'],
-        ];
-
-        $user = [
-
-        ];
-
-        $response = [
-            "message" => "Login Sukses"
-        ];
-        // Kirimkan responsenya ke view
-    }
+  function index()
+  {
+      View::render("Home/Index", [
+          "title" => "PHP Login Management",
+      ]);
+  }
 }
--- php-login-management/app/Middleware/MiddleWare.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ProgrammerZamanNow\Belajar\PHP\MVC\Middleware;
-
-interface MiddleWare
-{
-    function before(): void;
-}
