a19edb8889c99083aaaa49e980d8545dc9b01b28
adenafil <nafilie9@gmail.com>
2024-02-28 21:50:12 +0700

    274. Unit Test User Logout Controller

--- php-login-management/test/Controller/UserControllerTest.php
+++ php-login-management/test/Controller/UserControllerTest.php
@@ -17,9 +17,11 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service {
 namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller {
     use PHPUnit\Framework\TestCase;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
 
     class UserControllerTest extends TestCase
     {
@@ -179,6 +181,28 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller {
             $this->expectOutputRegex("[Password]");
             $this->expectOutputRegex("[Id or password is wrong]");
 
+        }
+
+        public function testLogout()
+        {
+            $user = new User();
+            $user->id = 'ade';
+            $user->password = password_verify("rahasia", PASSWORD_BCRYPT);
+            $user->name = 'Ade';
+
+            $this->userRepository->save($user);
+
+            $session = new Session();
+            $session->id = uniqid();
+            $session->userId = $user->id;
+            $this->sessionRepository->save($session);
+
+            $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+
+            $this->userController->logout();
+
+            $this->expectOutputRegex('[Location: /]');
+            $this->expectOutputRegex('[X-PZN-SESSION: ]');
         }
     }
 
