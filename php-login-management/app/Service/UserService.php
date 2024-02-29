e34a597b9431dcbd3405650856b6c111f003e224
adenafil <nafilie9@gmail.com>
2024-02-29 21:45:33 +0700

    279. User Profile Update Service

--- php-login-management/app/Service/UserService.php
+++ php-login-management/app/Service/UserService.php
@@ -7,6 +7,8 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginRequest;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginResponse;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserProfileUpdateRequest;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserProfileUpdateResponse;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterReponse;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
@@ -88,6 +90,44 @@ class UserService
         ) {
             throw new ValidationException("Id, Password can not not be blank or null");
         }
+    }
 
+    /**
+     * @throws ValidationException
+     */
+    private function updateProfile(UserProfileUpdateRequest $request): UserProfileUpdateResponse
+    {
+        $this->validateUserProfileUpdateRequest($request);
+        try {
+            Database::beginTransaction();
+
+            $user = $this->userRepository->findById($request->id);
+            if ($user == null) {
+                throw new ValidationException("User is not found");
+            }
+
+            $this->userRepository->update($user);
+
+            Database::commitTransaction();
+
+            $response = new UserProfileUpdateResponse();
+            $response->user = $user;
+            return $response;
+
+        } catch (\Exception $exception) {
+            Database::rollbackTransaction();
+            throw $exception;
+        }
+    }
+
+    private function validateUserProfileUpdateRequest(UserProfileUpdateRequest $request)
+    {
+        if (
+            $request->id = null || $request->name == null ||
+            trim($request->id) == "" || trim($request->name) == ""
+        )
+        {
+            throw new ValidationException("Id, Name can not be blank");
+        }
     }
 }
