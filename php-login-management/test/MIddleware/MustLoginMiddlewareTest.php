655991c8d709f51d433f071f05a7efd256991ec1
adenafil <nafilie9@gmail.com>
2024-02-29 20:48:34 +0700

    276. Unit Test Session Middleware

--- php-login-management/test/MIddleware/MustLoginMiddlewareTest.php
+++ php-login-management/test/MIddleware/MustLoginMiddlewareTest.php
@@ -1,10 +1,66 @@
 <?php
 
-namespace ProgrammerZamanNow\Belajar\PHP\MVC\MIddleware;
-
-use PHPUnit\Framework\TestCase;
-
-class MustLoginMiddlewareTest extends TestCase
-{
-
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\App {
+    function header(string $value)
+    {
+        echo $value;
+    }
 }
+
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\MIddleware{
+    use PHPUnit\Framework\TestCase;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
+
+    class MustLoginMiddlewareTest extends TestCase
+    {
+        private MustLoginMiddleware $middleware;
+        private UserRepository $userRepository;
+        private SessionRepository $sessionRepository;
+
+        protected function setUp(): void
+        {
+            $this->middleware = new MustLoginMiddleware();
+            putenv("mode=test");
+
+            $this->userRepository = new UserRepository(Database::getConnection());
+            $this->sessionRepository = new SessionRepository(Database::getConnection());
+
+            $this->sessionRepository->deleteAll();
+            $this->userRepository->deleteAll();
+        }
+
+        public function testBeforeGuest()
+        {
+            $this->middleware->before();
+            $this->expectOutputRegex("[Location: /users/login]");
+        }
+
+        public function testBeforeLoginUser()
+        {
+            $user = new User();
+            $user->id = 'ade';
+            $user->name = 'Ade';
+            $user->password = 'rahasia';
+
+            $this->userRepository->save($user);
+
+            $session = new Session();
+            $session->id = uniqid();
+            $session->userId = $user->id;
+            $this->sessionRepository->save($session);
+
+            $_COOKIE[SessionService::$COOKIE_NAME] = $session->id;
+
+            $this->middleware->before();
+            $this->expectOutputRegex("[]");
+        }
+
+    }
+}
+
